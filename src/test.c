3f2649c45834a66bf30d851097c99c38e8f3f438
Corentin Delcourt <codl@codl.fr>
2013-01-31 23:02:10 +0100

    return of the cursor

--- src/test.c
+++ src/test.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
+#include <signal.h>
 #include "screen.h"
+#include <errno.h>
+
+void signalCatch(int sig){
+    setTermOpts(1, 1, 1);
+    exit(0);
+}
 
 int main(int argc, char* argv){
     char input;
     int x=1, y=1;
     int width, height;
+    struct sigaction sa;
+
     clearScreen();
+
+    sa.sa_handler = &signalCatch;
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGQUIT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
+
     setTermOpts(0, 0, 0);
     while((input = getchar()) != EOF){
         getScreenSize(&width, &height);
@@ -37,6 +53,7 @@ int main(int argc, char* argv){
                 x--; y++;
                 break;
             case 'q':
+                setTermOpts(1, 1, 1);
                 return 0;
         }
         if(x > width){ x = width; }
